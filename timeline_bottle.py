3f3b9d317f23f487fbd78bfc0f95b1ab6392861a
root <root@DESKTOP-GOCSHMN.localdomain>
2021-03-09 18:35:56 -0800

    database config

--- timeline_bottle.py
+++ timeline_bottle.py
@@ -5,27 +5,59 @@ from bottle import request, response
 from bottle import post, get, put, delete
 import bottle
 
+app = bottle.Bottle()
+plugin = bottle.ext.sqlite.Plugin(dbfile ='/db/Posts.db')
 
+app.install(plugin))
+
+def json_error_handler(res):
+    if res.content_type == 'application/json':
+        return res.body
+    res.content_type = 'application/json'
+    if res.body == 'Unknown Error.':
+        res.body = bottle.HTTP_CODES[res.status_code]
+    return bottle.json_dumps({'error': res.body})
+
+
+app.default_error_handler = json_error_handler
+
+def query(db, sql, args=(), one=False):
+    cur = db.execute(sql, args)
+    rv = [dict((cur.description[idx][0], value)
+          for idx, value in enumerate(row))
+          for row in cur.fetchall()]
+    cur.close()
+
+    return (rv[0] if rv else None) if one else rv
+
+
+def execute(db, sql, args=()):
+    cur = db.execute(sql, args)
+    id = cur.lastrowid
+    cur.close()
+
+    return id
 
 
 @get('/timeline/usertimeline/<username>/')
 def getUserTimeline(username):
+    myPosts = query(db, 'SELECT message FROM Posts WHERE username = ? LIMIT 25;', [username])
+    # list_obj=[]
 
-    list_obj=[]
-
-    response.headers['Content-Type'] = 'application/json'
-    return json.dump({'timeline':str(list_obj)})
-
+    # response.headers['Content-Type'] = 'application/json'
+    # return json.dump({'timeline':str(list_obj)})
+    return {'posts' : myPosts}
 
     
        
 @get('/timeline/usertimeline/')
 def getPublicTimeline():
-    user_obj=[]
-    list_obj=[]
+    # user_obj=[]
+    # list_obj=[]
 
-    response.headers['Content-Type'] = 'application/json'
-    return json.dump({'timeline':str(user_obj)})
+    # response.headers['Content-Type'] = 'application/json'
+    # return json.dump({'timeline':str(user_obj)})
+    return "timeline"
 
 @get('/timeline/hometimeline/<username>/')
 def getUserTimeline(username):
--- user_bottle.py
+++ user_bottle.py
@@ -6,10 +6,41 @@ from bottle import post, get, put, delete
 import bottle
 from bottle.ext import sqlite
 
+app = bottle.Bottle()
+plugin = bottle.ext.sqlite.Plugin(dbfile ='/db/Users.db')
 
+app.install(plugin))
+
+def json_error_handler(res):
+    if res.content_type == 'application/json':
+        return res.body
+    res.content_type = 'application/json'
+    if res.body == 'Unknown Error.':
+        res.body = bottle.HTTP_CODES[res.status_code]
+    return bottle.json_dumps({'error': res.body})
+
+
+app.default_error_handler = json_error_handler
+
+def query(db, sql, args=(), one=False):
+    cur = db.execute(sql, args)
+    rv = [dict((cur.description[idx][0], value)
+          for idx, value in enumerate(row))
+          for row in cur.fetchall()]
+    cur.close()
+
+    return (rv[0] if rv else None) if one else rv
+
+
+def execute(db, sql, args=()):
+    cur = db.execute(sql, args)
+    id = cur.lastrowid
+    cur.close()
+
+    return id
 
 @post('/users/')
-def user_creation():
+def createUser():
     '''Handles name creation'''
     try:
         # parse input data
@@ -22,7 +53,13 @@ def user_creation():
         username=data['username']  
         password=data['password']
         email=data['email']
+        row = query(db, 'INSERT INTO Users (username, email, password) VALUES (?, ?, ?);'. [username], [password], [email])
+
+        if not row:
+            abort(404)
+
         response.status = "200 OK"
+
     except:
         #response.headers['Content-Type'] = 'application/json'
         #print("hi")
@@ -88,7 +125,7 @@ def deletefollower():
 @get('/users/<username>/follower')
 def getFollowers(username):
     
-    return sql_data
+    return "hit"
 
 if __name__ == '__main__':
     bottle.run(host = '127.0.0.1', port = 8000) 
