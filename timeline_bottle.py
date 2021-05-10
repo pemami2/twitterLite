30973cc4113f7624eb818ece1e3b7cbc45ecfdbe
pemami2 <root@DESKTOP-GOCSHMN.localdomain>
2021-05-10 14:25:29 -0700

    functional message queue

--- timeline_bottle.py
+++ timeline_bottle.py
@@ -9,18 +9,21 @@ import bottle
 import json
 from bottle import get, post, error, abort, request, response, HTTPResponse
 from bottle.ext import sqlite
+from redis import Redis
+from rq import Queue
+from rq.job import Job
+import time
+import os
 
 if not sys.warnoptions:
     import warnings
     for warning in [DeprecationWarning, ResourceWarning]:
         warnings.simplefilter('ignore', warning)
 
-
 app = bottle.default_app()
 plugin = bottle.ext.sqlite.Plugin(dbfile ='db/Posts.db')
-
 app.install(plugin)
-
+    
 def json_error_handler(res):
     if res.content_type == 'application/json':
         return res.body
@@ -30,7 +33,7 @@ def json_error_handler(res):
     return bottle.json_dumps({'error': res.body})
 
 
-app.default_error_handler = json_error_handler
+#app.default_error_handler = json_error_handler
 
 def query(db, sql, args=(), one=False):
     cur = db.execute(sql, args)
@@ -49,51 +52,7 @@ def missingFields(required, posted):
     else:
         return False
 
-
-@get('/timeline/user/<username>/')
-def getUserTimeline(username,db):
-    myPosts = query(db, 'SELECT message FROM Posts WHERE username = ? LIMIT 25;', [username])
-
-    return {'posts' : myPosts}
-
-    
-       
-@get('/timeline/')
-def getPublicTimeline(db):
-
-    posts = query(db, 'SELECT username, message FROM Posts LIMIT 25')
-
-    return {'posts' : posts}
-
-@get('/timeline/home/<username>/')
-def getUserTimeline(username, db):
-
-    
-    response = requests.get('http://localhost:5000/users/' + username + '/follow/')
-    print(response.text)
-    
-
-    data = json.loads(response.text) 
-    following = list()
-
-    for x in data['followers']:
-        following.append(x.get('Follow'))
-
-    if len(following) == 1:
-        posts = query(db, 'SELECT username, message FROM Posts WHERE Username = ? LIMIT 25;', [following[0]])
-
-    else:
-        following_tuple = tuple(following)
-        posts = query(db, 'SELECT username, message FROM Posts WHERE Username IN {} LIMIT 25;'.format(following_tuple))
-
-
-    return {'Posts': posts}
-
-@post('/timeline/')
-def user_creation(db):
-    '''Handles name creation'''
-
-    data = request.json
+def createPost(db, data):
 
     if data is not None:
         keys = data.keys()
@@ -105,19 +64,111 @@ def user_creation(db):
     if missing:
         abort(400, missing)
 
-    username = data['username']  
+    username = data['username']
     message = data['message']
 
-    try: 
+    try:
         result = query(db, 'INSERT INTO Posts (username, message) VALUES (?, ?);', [username, message])
         response.body = result
         response.status = 200
     except:
         abort(500)
 
-    return response.status  
+    return response.status
 
+def countHashtags(message):
+    r = Redis()
+    words = message.split()
+    hashtags = {}
+    for word in words:
+        if word[0] == '#':
+            hashtags[word] = hashtags.get(word, 0) + 1
 
-#if __name__ == '__main__':
-#    bottle.run(host = 'locahost', port = 8001) 
+    for tag in hashtags:
+        r.zincrby("hashtags", hashtags[tag], tag)
     
+    response.status = 200
+
+    return response.status
+
+@get('/timeline/user/<username>/')
+def getUserTimeline(username,db):
+    myPosts = query(db, 'SELECT message FROM Posts WHERE username = ? ORDER BY id DESC LIMIT 25;', [username])
+
+    return {'posts' : myPosts}
+
+    
+       
+@get('/timeline/')
+def getPublicTimeline(db):
+
+    posts = query(db, 'SELECT username, message FROM Posts ORDER BY id DESC LIMIT 25')
+
+    return {'posts' : posts}
+
+@get('/timeline/home/<username>/')
+def getUserTimeline(username, db):
+
+    
+    response = requests.get('http://localhost:5000/users/' + username + '/follow/')
+    
+    data = json.loads(response.text) 
+    following = list()
+
+    for x in data['followers']:
+        following.append(x.get('Follow'))
+
+    if len(following) == 1:
+        posts = query(db, 'SELECT username, message FROM Posts WHERE Username = ? ORDER BY id DESC LIMIT 25;', [following[0]])
+
+    else:
+        following_tuple = tuple(following)
+        posts = query(db, 'SELECT username, message FROM Posts WHERE Username IN {} ORDER BY id DESC LIMIT 25;'.format(following_tuple))
+
+
+    return {'Posts': posts}
+
+@post('/timeline/')
+def postQueue(db):
+    conn = Redis()
+    publish = Queue("publish", connection=conn)
+    trending = Queue("trending", connection=conn)
+    data = request.json
+
+    if data is not None:
+        keys = data.keys()
+    else:
+        keys = set()
+
+    missing = missingFields({'username', 'message'}, keys)
+
+    if missing:
+        abort(400, missing)
+    
+    publish_result = publish.enqueue(createPost(db, data))
+    trending_result = trending.enqueue(countHashtags(data['message']))
+    publishID = str(publish_result.id)
+    trendingID = str(trending_result.id)
+    publish_status = Job.fetch(publish_result.id, connection=conn)
+    trend_status = Job.fetch(trending_result.id, connection=conn)
+    
+    if publish_status.is_finished:
+        response.status = 200 
+    else:
+        response.status = 202
+    
+    return response
+
+@get('/timeline/trending/')
+def getHashTags():
+    r = Redis()
+    try:
+        trending = r.zrange("hashtags", 0, 24, True, True)
+        response.status = 200
+        return {'Trending Topics' : trending}
+    except:
+        responsebody = {'Error' : 'Unable to retrieve trending topics'}
+        response.status = 500
+
+    return response
+
